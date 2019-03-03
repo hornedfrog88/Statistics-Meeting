b7aa88b89eeec6e53ffa4ecfaced8532e89f2ac3
Rich McGowan <rmcgowan@berkeley.edu>
2019-03-02 16:34:28 -0800

    03/2/2019 Commit

--- suicide_analysis_female.R
+++ suicide_analysis_female.R
@@ -1,20 +1,23 @@
 library(readr)
 library(dplyr)
 library(ggplot2)
-suicide_df <- read_csv("C:/Users/Richard/AppData/Local/GitHubDesktop/app-1.4.2/Statistics-Meeting/WHO_new_suicide_statistics.csv")
+setwd("C:/Users/Richard/AppData/Local/GitHubDesktop/app-1.4.2/Statistics-Meeting")
+suicide_df <- read_csv("WHO_new_suicide_statistics.csv")
+names(suicide_df)[7] <- "suicides_per_100K"
 suicide_df <- suicide_df %>% mutate(suicide_pct_pop = suicides_no/population)
 
 suicide_us_df <- suicide_df %>% filter(country=='United States')
+
 # create data frame that shows female suicides as a percentage of the population
 # by year since 1990 in the United States
 suicide_us_fem_since1990_df <-  suicide_us_df %>% 
-  filter(sex == 'female', year > 1989) %>%
-  select(year, sex, population, suicide_pct_pop)%>% 
+  filter(sex == 'female', year > 1989 & year < 2015) %>%
+  select(year, sex, population, suicides_per_100K, suicide_pct_pop)%>% 
   group_by(year) %>% 
-  summarise(total_pop = sum(population)/1000000,total_pct_pop = sum(suicide_pct_pop))  
+  summarise(total_pop = sum(population)/1000000,total_suicides_100K_pop = sum(suicides_per_100K))  
 
-ggplot(suicide_us_fem_since1990_df,aes(x=year,y=total_pct_pop))+geom_point()+
-  ggtitle("Number of Female Suicides (in the U.S.) \n per Percent of Population by Year Since 1990")+ylab("Suicides as a Percentage of Population")+
+ggplot(suicide_us_fem_since1990_df,aes(x=year,y=total_suicides_100K_pop))+geom_point()+
+  ggtitle("Number of Female Suicides (in the U.S.) \n per 100K Population by Year Since 1990")+ylab("Suicides per 100K Population")+
   theme(
     plot.title = element_text(color="red", size=14, face="bold.italic"),
     axis.title.x = element_text(color="blue", size=12, face="bold"),
@@ -28,28 +31,31 @@ ggplot(suicide_us_fem_since1990_df,aes(x=year,y=total_pop))+geom_point()+
     axis.title.y = element_text(color="blue", size=12, face="bold")
   )
 
-suicide_gb_df <- suicide_df %>% filter(country=='United Kingdom')
-# create data frame that shows female suicides as a percentage of the population
-# by year since 1990 in Great Britain
-suicide_gb_fem_since1990_df <-  suicide_gb_df %>% 
-  filter(sex == 'female', year > 1989) %>%
-  select(year, sex, population, suicide_pct_pop)%>% 
+develop_countries <- c('Germany')
+suicide_devworld_df <- suicide_df %>% filter(country %in% 
+                                         develop_countries)
+suicide_devworld_df$suicides_per_100K <- suicide_devworld_df$suicides_per_100K/length(develop_countries)
+suicide_devworld_fem_since1990_df <-  suicide_devworld_df %>% 
+  filter(sex == 'female', year > 1989 & year < 2015) %>%
+  select(year, sex, population, suicides_per_100K)%>% 
   group_by(year) %>% 
-  summarise(total_pop = sum(population)/1000000,total_pct_pop = sum(suicide_pct_pop))  
+  summarise(total_pop = sum(population)/1000000,total_suicides_100K_pop = sum(suicides_per_100K))  
 
-ggplot(suicide_gb_fem_since1990_df,aes(x=year,y=total_pct_pop))+geom_point()+
-  ggtitle("Number of Female Suicides (in Great Britain) \n per Percent of Population by Year Since 1990")+ylab("Suicides as a Percentage of Population")+
+ggplot(suicide_devworld_fem_since1990_df,aes(x=year,y=total_suicides_100K_pop))+geom_point()+
+  ggtitle("Number of Female Suicides (in western nations) \n per Percent of Population by Year Since 1990")+ylab("Suicides as a Percentage of Population")+
   theme(
     plot.title = element_text(color="red", size=14, face="bold.italic"),
     axis.title.x = element_text(color="blue", size=12, face="bold"),
     axis.title.y = element_text(color="blue", size=12, face="bold")
   )
-ggplot(suicide_gb_fem_since1990_df,aes(x=year,y=total_pop))+geom_point()+
-  ggtitle("Female Population (in Great Britain) by Year Since 1990")+ylab("Total Population in Millions")+
+ggplot(suicide_devworld_fem_since1990_df,aes(x=year,y=total_pop))+geom_point()+
+  ggtitle("Female Population (in western nations) by Year Since 1990")+ylab("Total Population in Millions")+
   theme(
     plot.title = element_text(color="red", size=14, face="bold.italic"),
     axis.title.x = element_text(color="blue", size=12, face="bold"),
     axis.title.y = element_text(color="blue", size=12, face="bold")
   )
 
+mean_us <- mean(suicide_us_fem_since1990_df$total_suicides_100K_pop)
+mean_devworld <- mean(suicide_devworld_fem_since1990_df$total_suicides_100K_pop)/length(develop_countries)
 
